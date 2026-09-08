616551352a321e612bde6aa49672e30cd34eac77
Grace Wilson <giw6@cornell.edu>
2026-09-08 14:52:31 +0000

    first part final

--- src/components/SoundButton.vue
+++ src/components/SoundButton.vue
@@ -25,7 +25,7 @@ function toggleSound() {
 </script>
 
 <template>
-  <button type="button" class="sound-button" :class="{ playing: isPLaying }" v-on:click="toggleSound">
+  <button type="button" class="sound-button" :class="{ playing: isPlaying }" v-on:click="toggleSound">
     <img v-if="isPlaying" class="status" src="/icons/speaker-active.svg" alt="" width="16" height="16" />
     <img v-else class="status" src="/icons/speaker-mute.svg" alt="" width="16" height="16" />
     <i>
